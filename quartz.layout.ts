6fd690b33a522d98376643eb77e304bd36c2784a
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2025-06-01 14:47:17 +0400

    fix(quartz.layout.ts): move toc above graph

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -66,8 +66,8 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
 //  Component.DesktopOnly(Component.Darkmode()),
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.Graph(),
     // Component.Backlinks(),
   ],
 }
