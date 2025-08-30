1ec0133dde40e84fdfe30cc672ba6c59bddd5590
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2025-08-30 14:49:48 +0400

    Comment out unused links in LinksHeader component

--- quartz/components/LinksHeader.tsx
+++ quartz/components/LinksHeader.tsx
@@ -22,15 +22,15 @@ export default (() => {
             <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Brain/Color/brain_color.svg"></img>
             <a href="/life">Life</a>
           </span>
-          <span>
+{/*           <span>
             <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/14053d26005b9afe3ce0e020862ed5ab2335f989/assets/Light%20bulb/Color/light_bulb_color.svg"></img>
             <a href="/lighting">Lighting</a>
-          </span>
+          </span> */}
           <span>
             <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/14053d26005b9afe3ce0e020862ed5ab2335f989/assets/File%20folder/Flat/file_folder_flat.svg"></img>
             <a href="/posts">Posts</a>
           </span>
-          {/* <span>
+{/*           <span>
             <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Laptop/Color/laptop_color.svg"></img>
             <a href="https://github.com/nkapila6">Github</a>
           </span> */}
