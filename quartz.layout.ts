d0f016bd15beb26452ce546570448ec36b3fce99
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2026-07-17 12:21:05 +0400

    feat: show date on posts via ContentMeta

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -36,7 +36,7 @@ export const defaultContentPageLayout: PageLayout = {
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
-    // Component.ContentMeta(),
+    Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
